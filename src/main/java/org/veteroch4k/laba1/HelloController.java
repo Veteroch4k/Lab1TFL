7b0e46f392071111c3f946adfa597bbec27d9980
Veteroch4k <indefinite05@mail.ru>
2026-02-28 00:08:16 +0700

    start implementing menuBar functions

--- src/main/java/org/veteroch4k/laba1/HelloController.java
+++ src/main/java/org/veteroch4k/laba1/HelloController.java
@@ -1,14 +1,26 @@
 package org.veteroch4k.laba1;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextArea codeEditorArea;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onExitClick(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
     }
+
+    // тут будет логика для файликов
+    @FXML
+    public void onOpenFileClick(ActionEvent event) {
+        System.out.println("Когда-нибудь здесь откроется файл");
+    }
+
+
 }
